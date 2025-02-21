9988d36d87a6d7d7750e82ae676577b5526b6b25
ninja <yinghao2151@gmail.com>
2025-02-21 19:29:01 +0800

    Created FANETSimulator files

---
+++ FANETSimulator.h
@@ -0,0 +1,23 @@
+#ifndef FANET_SIMULATOR_H
+#define FANET_SIMULATOR_H
+
+// Helper Classes Includes
+#include "ns3/fanet-module.h"
+
+namespace ns3 
+{
+    class FANETSimulator
+    {
+        private:
+            // Variables and Vectors
+
+            // Helpers
+
+            // Methods
+
+        public:
+
+    };
+}
+
+#endif
