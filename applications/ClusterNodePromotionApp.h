d90980e6a55ef8b41878742111a8f63331dda03f
ninja <yinghao2151@gmail.com>
2025-02-26 22:20:18 +0800

    continue tmr

--- applications/ClusterNodePromotionApp.h
+++ applications/ClusterNodePromotionApp.h
@@ -1,11 +1,36 @@
-#ifndef CLUSTER_NODE_PROMOTION_H
-#define CLUSTER_NODE_PROMOTION_H
+#ifndef CLUSTER_NODE_PROMOTION_APP_H
+#define CLUSTER_NODE_PROMOTION_APP_H
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/applications-module.h"
 
+namespace ns3
+{
+    class ClusterNodePromotionApp : public Application
+    {
+        public:
 
+            ClusterNodePromotionApp();
+            virtual ~ClusterNodePromotionApp();
+
+            void SetUp(Ptr<Node> GDTNode, Ipv4Address GDTIp, uint16_t port);
+
+        private:
+            
+            Ptr<Socket> m_socket;
+            Ipv4Address m_GDTIp;
+            uint16_t m_port;
+            bool isClusterHead;
+            EventId m_sendEvent; 
+            
+            virtual void StartApplication() override;
+            virtual void StopApplication() override;
+
+            void HandleRead(Ptr<Socket> socket);
+            void NotifyGDT();
+    };
+}
 
 #endif
