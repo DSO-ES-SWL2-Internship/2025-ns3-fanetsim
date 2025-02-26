d9d3a18d5a7ce5c6568eff63943d08132ba0b90e
ninja <yinghao2151@gmail.com>
2025-02-26 21:59:02 +0800

    file rename

---
+++ applications/ClusterNodePromotionApp.h
@@ -0,0 +1,11 @@
+#ifndef CLUSTER_NODE_PROMOTION_H
+#define CLUSTER_NODE_PROMOTION_H
+
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/applications-module.h"
+
+
+
+#endif
